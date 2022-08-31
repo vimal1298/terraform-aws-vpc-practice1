04a03eb87f331f6b802df8c8140c4c82d97de675
vimal1298 <110822479+vimal1298@users.noreply.github.com>
2022-08-31 20:31:11 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "var_vpc_cidr" {
+  type        = string
+}
+
+variable "var_subnet_cidr" {
+  type        = string
+}
+
+variable "var_available_zone" {
+type  = string
+}
+
+variable "instance_type" {}
+
+variable "ami" {}
