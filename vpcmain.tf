a23fc1b3bcf8a47f8202f5de64da07eb7ddac8b0
vimal1298 <110822479+vimal1298@users.noreply.github.com>
2022-09-01 11:04:26 +0530

    Update vpcmain.tf

--- vpcmain.tf
+++ vpcmain.tf
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "web_gw" {
 resource "aws_subnet" "web_subnet" {
   vpc_id     = aws_vpc.web_vpc.id
   cidr_block = var.var_subnet_cidr #"10.0.1.0/24"
-  availability_zone = var.var_available_zone #"us-west-2a"
+  #availability_zone = var.var_available_zone #"us-west-2a"
 
   tags = {
     Name = "web_subnet"
